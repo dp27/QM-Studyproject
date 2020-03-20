33e833f2e83efcba5ee70092452e4ca314866e8b
dp27 <60425714+dp27@users.noreply.github.com>
2020-03-20 18:25:53 +0100

    Update MAP_chirchik.R

--- MAP_chirchik.R
+++ MAP_chirchik.R
@@ -46,16 +46,32 @@ names(chirchik.drainage)[1]<-paste("year.chir")
 names(chirchik.drainage)[2]<-paste("Month")
 names(chirchik.drainage)[3]<-paste("Q")
 chirchik.drainage$V4<-NULL
+
+chirchik.snow2<-chirchik.snow
+chirchik.snow2$V5<-NULL
+chirchik.snow2$Month[chirchik.snow2$Month == 1] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 2] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 4] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 5] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 6] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 7] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 8] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 9] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 10] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 11] <- NA
+chirchik.snow2$Month[chirchik.snow2$Month == 12] <- NA
+chirchik.snow2$V4[chirchik.snow2$V4 == -9]<- NA
+omitted_chirchik.snow<- na.omit(chirchik.snow2)
+omitted_chirchik.snow2<-aggregate(omitted_snow[, 2:4], list(omitted_snow$year), mean)
+names(omitted_chirchik.snow2)[1]<-paste("year.chir")
+
 #data preperation for merge
 ##Obsolete
 
-chir.total<-merge(chirchik.snow,chirchik.drainage,by=c("Month","year.chir"))
+chir.total<-merge(chirchik.drainage,omitted_chirchik.snow2,by=c("year.chir"))
 chir.total$V4[chir.total$V4 == -9]<- NA
 chir.total$V5 <- NULL
 omitted<-na.omit(chir.total)
-
-chir.Monthly.mean<-aggregate(chir.total[, 5:7], list(chir.total$Month), mean)
-chir.year.chirly.mean<-aggregate(chir.total[, 5:7], list(chir.total$year.chir), mean)
 
 ##data preperation for seasonal.chir mean
 chir.total2<-chir.total
@@ -76,7 +92,7 @@ april.year.chir$Month[april.year.chir$Month == 7] <- NA
 april.year.chir$Month[april.year.chir$Month == 8] <- NA
 april.year.chir$Month[april.year.chir$Month == 9] <- NA
 april.year.chir <- na.omit(april.year.chir)
-seasonal.chir.mean.april<- aggregate(april.year.chir[, 5:6], list(april.year.chir$year.chir), mean)
+seasonal.chir.mean.april<- aggregate(april.year.chir[, 3:6], list(april.year.chir$year.chir), mean)
 seasonal.chir.mean.april$Month<-c("April") 
 
 may.year.chir<-omit_chir.total
@@ -86,7 +102,7 @@ may.year.chir$Month[may.year.chir$Month == 7] <- NA
 may.year.chir$Month[may.year.chir$Month == 8] <- NA
 may.year.chir$Month[may.year.chir$Month == 9] <- NA
 may.year.chir <- na.omit(may.year.chir)
-seasonal.chir.mean.may<- aggregate(may.year.chir[, 5:6], list(may.year.chir$year.chir), mean)
+seasonal.chir.mean.may<- aggregate(may.year.chir[, 3:6], list(may.year.chir$year.chir), mean)
 seasonal.chir.mean.may$Month<-c("May") 
 
 june.year.chir<-omit_chir.total
@@ -96,7 +112,7 @@ june.year.chir$Month[june.year.chir$Month == 7] <- NA
 june.year.chir$Month[june.year.chir$Month == 8] <- NA
 june.year.chir$Month[june.year.chir$Month == 9] <- NA
 june.year.chir <- na.omit(june.year.chir)
-seasonal.chir.mean.june<- aggregate(june.year.chir[, 5:6], list(june.year.chir$year.chir), mean)
+seasonal.chir.mean.june<- aggregate(june.year.chir[, 3:6], list(june.year.chir$year.chir), mean)
 seasonal.chir.mean.june$Month<-c("June") 
 
 july.year.chir<-omit_chir.total
@@ -106,7 +122,7 @@ july.year.chir$Month[july.year.chir$Month == 6] <- NA
 july.year.chir$Month[july.year.chir$Month == 8] <- NA
 july.year.chir$Month[july.year.chir$Month == 9] <- NA
 july.year.chir <- na.omit(july.year.chir)
-seasonal.chir.mean.july<- aggregate(july.year.chir[, 5:6], list(july.year.chir$year.chir), mean)
+seasonal.chir.mean.july<- aggregate(july.year.chir[, 3:6], list(july.year.chir$year.chir), mean)
 seasonal.chir.mean.july$Month<-c("July") 
 
 august.year.chir<-omit_chir.total
@@ -116,7 +132,7 @@ august.year.chir$Month[august.year.chir$Month == 6] <- NA
 august.year.chir$Month[august.year.chir$Month == 7] <- NA
 august.year.chir$Month[august.year.chir$Month == 9] <- NA
 august.year.chir <- na.omit(august.year.chir)
-seasonal.chir.mean.august<- aggregate(august.year.chir[, 5:6], list(august.year.chir$year.chir), mean)
+seasonal.chir.mean.august<- aggregate(august.year.chir[, 3:6], list(august.year.chir$year.chir), mean)
 seasonal.chir.mean.august$Month<-c("August") 
 
 september.year.chir<-omit_chir.total
@@ -126,19 +142,18 @@ september.year.chir$Month[september.year.chir$Month == 6] <- NA
 september.year.chir$Month[september.year.chir$Month == 7] <- NA
 september.year.chir$Month[september.year.chir$Month == 8] <- NA
 september.year.chir <- na.omit(september.year.chir)
-seasonal.chir.mean.september<- aggregate(september.year.chir[, 5:6], list(september.year.chir$year.chir), mean)
+seasonal.chir.mean.september<- aggregate(september.year.chir[, 3:6], list(september.year.chir$year.chir), mean)
 seasonal.chir.mean.september$Month<-c("September") 
 
 year.chirly.mean.Month<- rbind(seasonal.chir.mean.april,seasonal.chir.mean.may,seasonal.chir.mean.june,seasonal.chir.mean.july,seasonal.chir.mean.august,seasonal.chir.mean.september)
 #Cbind aber davor Group1 umbennen zu den jeweiligen Monatsnamen
-seasonal.chir.mean.year.chir<- aggregate(omit_chir.total[, 5:6], list(omit_chir.total$year.chir), mean)
+seasonal.chir.mean.year.chir<- aggregate(omit_chir.total[, 3:6], list(omit_chir.total$year.chir), mean)
+seasonal.chir.mean.year.chir$name <- c("chir")
+
 seasonal.chir.mean.Month<- aggregate(omit_chir.total[, 5:6], list(omit_chir.total$Month), mean)
 
 ##model selection
 library(ggplot2)
-model.chir.total <- lm(data = omit_chir.total, Q~V4)
-model.seasonal.chir.year.chir<-lm(data = omit_chir.total, Q ~ V4)
-model.seasonal.chir.Month <-lm(data = seasonal.chir.mean.Month, Q ~ V4)
 ##model.seasonal.chir.Month is the good one
 ##NEU ab hier 25/02/2020
 lm.chir.september <- lm(data = september.year.chir, Q~V4)
@@ -151,9 +166,46 @@ lm.chir.vp <- lm(data = omit_chir.total, Q~V4)
 
 library(tidyr)
 library(dplyr)
+pb.chir.september <- lm.chir.september %>%
+  predict(., interval = 'prediction') %>%
+  as.data.frame() %>% mutate(x = sample(september.year.chir$year.chir))
+pb.september.chir<-aggregate(prediction.chir.september[, 1:3], list(prediction.chir.september$x), mean)
+pb.september.chir$month<-c("September")
+
+pb.chir.august <- lm.chir.august %>%
+  predict(., interval = 'prediction') %>%
+  as.data.frame() %>% mutate(x = sample(august.year.chir$year.chir))
+pb.august.chir<-aggregate(prediction.chir.august[, 1:3], list(prediction.chir.august$x), mean)
+pb.august.chir$month<-c("august")
+
+pb.chir.july <- lm.chir.july %>%
+  predict(., interval = 'prediction') %>%
+  as.data.frame() %>% mutate(x = sample(july.year.chir$year.chir))
+pb.july.chir<-aggregate(prediction.chir.july[, 1:3], list(prediction.chir.july$x), mean)
+pb.july.chir$month<-c("july")
+
+pb.chir.june <- lm.chir.june %>%
+  predict(., interval = 'prediction') %>%
+  as.data.frame() %>% mutate(x = sample(june.year.chir$year.chir))
+pb.june.chir<-aggregate(prediction.chir.june[, 1:3], list(prediction.chir.june$x), mean)
+pb.june.chir$month<-c("june")
+
+pb.chir.may <- lm.chir.may %>%
+  predict(., interval = 'prediction') %>%
+  as.data.frame() %>% mutate(x = sample(may.year.chir$year.chir))
+pb.may.chir<-aggregate(prediction.chir.may[, 1:3], list(prediction.chir.may$x), mean)
+pb.may.chir$month<-c("may")
+
+pb.chir.april <- lm.chir.april %>%
+  predict(., interval = 'prediction') %>%
+  as.data.frame() %>% mutate(x = sample(april.year.chir$year.chir))
+pb.april.chir<-aggregate(prediction.chir.april[, 1:3], list(prediction.chir.april$x), mean)
+pb.april.chir$month<-c("april")
+
+##confidence intervall
 prediction.chir.september <- lm.chir.september %>%
   predict(., interval = 'confidence') %>%
-  as.data.frame() %>% mutate(x = sample(september.year.chir$year,chir))
+  as.data.frame() %>% mutate(x = sample(september.year.chir$year.chir))
 mean.p.september.chir<-aggregate(prediction.chir.september[, 1:3], list(prediction.chir.september$x), mean)
 mean.p.september.chir$month<-c("September")
 
@@ -195,7 +247,7 @@ chir.mean.p.vp<-aggregate(prediction.chir.vp[, 1:3], list(prediction.chir.vp$x),
 pred.all.months.chir <- rbind(mean.p.april.chir,mean.p.may.chir,mean.p.june.chir,mean.p.july.chir,mean.p.august.chir,mean.p.september.chir)
 mean.pred.month.chir <- aggregate(pred.all.months.chir[, 2:4], list(pred.all.months.chir$month), mean)
 
-p.chir<-ggplot(seasonal.chir.mean.year.chir, aes(x = Group.1, y = Q)) + geom_line() + geom_line(data = chir.mean.p.vp, aes(x = Group.1, y = x, col = "prediction.chir")) + xlab("year")
+p.chir<-ggplot(seasonal.chir.mean.year.chir, aes(x = Group.1, y = Q)) + geom_line() + geom_line(data = chir.mean.p.vp, aes(x = Group.1, y = fit, col = "prediction.chir")) + xlab("year")
 p.chir
 summary(lm.chir.vp)
 ggplot(seasonal.chir.mean.year, aes(x = Group.1, y = Q)) + geom_line() + geom_line(data = prediction.chir.september, aes(x = x, y = fit, col = "september"))+ geom_line(data = prediction.chir.august, aes(x = x, y = fit, col = "august")) + geom_line(data = prediction.chir.july, aes(x = x, y = fit, col = "july")) +geom_line(data = prediction.chir.june, aes(x = x, y = fit, col = "june"))
